09550fbd9f3db0982e3e6db97a79add84fb01398
Katharine Berry <katharine@ponyfest.horse>
2020-11-04 18:43:14 -0800

    PVCF20 customisation

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
                 use: [MiniCssExtractPlugin.loader, 'css-loader'],
             },
             {
-                test: /\.woff2?$/,
+                test: /\.woff2?|\.ttf$/,
                 use: 'file-loader',
             },
         ],
